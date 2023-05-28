35dcd4c7beca2958768873bd2c7c698dfc3bc324
nandini123886 <99880489+nandini123886@users.noreply.github.com>
2023-05-28 11:49:12 +0530

    Update campgrounds.js

--- routers/campgrounds.js
+++ routers/campgrounds.js
@@ -5,7 +5,7 @@ const middleWare = require("../middleware");
 
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.render("campgrounds/homepage");
+  res.render("./campgrounds/homepage");
 });
 router.get("/campgrounds", (req, res) => {
   try {
